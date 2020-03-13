7aa6027152531fec24ef39283e01d5a51a3a0969
azicks <emperon@rambler.ru>
2020-03-13 22:21:48 +0700

    001.1 Codecov

---
+++ multithreading/src/test/java/concurrent/ConcurrentOutputTest.java
@@ -0,0 +1,16 @@
+package concurrent;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConcurrentOutputTest {
+
+    /**
+     * Testing codecov.io
+     */
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
