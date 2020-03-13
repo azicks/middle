678aeaa1131bff061ac9b337c4438c7cabcdd7bb
azicks <emperon@rambler.ru>
2020-03-13 23:12:49 +0700

    001.1.1.2. Режим ожидания.[#251584]

---
+++ multithreading/src/main/java/concurrent/Wget.java
@@ -0,0 +1,25 @@
+package concurrent;
+
+public class Wget {
+    static private int index;
+    public static void main(String[] args) {
+        Thread thread = new Thread( () -> {
+            for (index = 0; index != 100; index++) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        while (thread.getState() != Thread.State.TERMINATED) {
+            System.out.print("\rLoading : " + index + "%");
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
