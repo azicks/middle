313ec3af45252900f3579a3b23060c2089356648
azicks <emperon@rambler.ru>
2020-03-25 11:36:51 +0700

    001.3.3. ThreadSafe динамический список [#251594] fix

--- multithreading/src/main/java/concurrent/SingleLockList.java
+++ multithreading/src/main/java/concurrent/SingleLockList.java
@@ -3,8 +3,8 @@ package concurrent;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.Queue;
 
 @ThreadSafe
 public class SingleLockList<T> implements Iterable<T> {
@@ -12,37 +12,37 @@ public class SingleLockList<T> implements Iterable<T> {
     @GuardedBy("this")
     private T[] data = (T[]) new Object[20];
     private int size = 0;
-    private int position = 0;
-    private int modCount = 0;
 
     public synchronized void add(T value) {
         if (size + 1 == data.length) {
             System.arraycopy(data, 0, data, 0, data.length + 20);
         }
         data[size++] = value;
-        modCount++;
     }
 
     public synchronized T get(int index) {
-        return data[position];
+        return data[index];
     }
 
     @Override
     public synchronized Iterator<T> iterator() {
-        return new Iterator<T>() {
-            int currentMods = modCount;
+        return new Iterator<>() {
+            final int currentSize = size;
+            @SuppressWarnings("unchecked")
+            final T[] currentData = (T[]) new Object[currentSize];
+            int position = 0;
+            {
+                System.arraycopy(data, 0, currentData, 0, currentSize);
+            }
 
             @Override
             public boolean hasNext() {
-                return position < size;
+                return position < currentSize;
             }
 
             @Override
             public synchronized T next() {
-                if (currentMods != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-                return data[position++];
+                return currentData[position++];
             }
         };
     }
